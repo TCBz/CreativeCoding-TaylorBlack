5fac1f5555eb900d307efdd773b39a684b6ede66
TCBz <taylor.c.black@gmail.com>
2016-09-16 18:54:07 -0400

    A bunch of shape tests from week 1 lecture

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
 function setup() {
-  println("Look, I can code!");
+  createCanvas(400,400);
+  strokeWeight(10);
+  rectMode(CENTER);
 }
 
+function draw() {
+  background(100);
 
+quad(10,10, 200,10, 390,390, 200,390);
+ellipse(100,100,100,350);
+ rect(200,200,100,100)
+ line(0,0,400,400);
+ triangle(400,0, 200,0, 400,400);
+ arc(300,300, 100,100, 0, PI, CHORD);
+}
